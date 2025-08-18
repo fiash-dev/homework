fba9f67c47608e9a7ad6ad52921a62469217bc76
fiash-dev <fiash099rnd@gmail.com>
2025-08-18 12:23:51 +0300

    Add files via upload

---
+++ MyHashSet_and_Students/src/MyArrayList.java
@@ -0,0 +1,92 @@
+import java.util.Arrays;
+import java.util.Collection;
+
+    public class MyArrayList<E> {
+        private static final int DEFAULT_CAPACITY = 10;
+        private Object[] elements;
+        private int size;
+
+        public MyArrayList() {
+            elements = new Object[DEFAULT_CAPACITY];
+            size = 0;
+        }
+
+        public boolean add(E element) {
+            ensureCapacity();
+            elements[size++] = element;
+            return true;
+        }
+
+        public void add(int index, E element) {
+            checkIndexForAdd(index);
+            ensureCapacity();
+
+            System.arraycopy(elements, index, elements, index + 1, size - index);
+            elements[index] = element;
+            size++;
+        }
+
+        public boolean addAll(Collection<? extends E> c) {
+            for (E element : c) {
+                add(element);
+            }
+            return !c.isEmpty();
+        }
+
+        @SuppressWarnings("unchecked")
+        public E get(int index) {
+            checkIndex(index);
+            return (E) elements[index];
+        }
+
+        public E remove(int index) {
+            checkIndex(index);
+            E oldValue = get(index);
+
+            int numMoved = size - index - 1;
+            if (numMoved > 0) {
+                System.arraycopy(elements, index + 1, elements, index, numMoved);
+            }
+
+            elements[--size] = null; // Помощь сборщику мусора
+            return oldValue;
+        }
+
+        public boolean remove(Object o) {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elements[i])) {
+                    remove(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        private void ensureCapacity() {
+            if (size == elements.length) {
+                int newCapacity = elements.length * 2;
+                elements = Arrays.copyOf(elements, newCapacity);
+            }
+        }
+
+        private void checkIndex(int index) {
+            if (index < 0 || index >= size) {
+                throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+            }
+        }
+
+        private void checkIndexForAdd(int index) {
+            if (index < 0 || index > size) {
+                throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return Arrays.toString(Arrays.copyOf(elements, size));
+        }
+    }
